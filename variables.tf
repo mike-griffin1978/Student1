11b525b6b48845462b8dcf1bd0822ca1c99d6b85
mike-griffin1978 <mike.griffin@eplus.com>
2023-06-03 21:37:22 +0000

    added everything

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+  default = "podx-vm-01"
+}
+
+variable "env" {
+  type = string
+  default = "SRE-LAB"
+}
+
+variable "owner" {
+  type = string
+  default = "POD23"
+}
+
+variable "key_pair_name" {
+  type = string
+  default = "sre_podx_key"
+}
